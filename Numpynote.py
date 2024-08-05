cc54be7d1a21a9e4caaa3517f085b068b8619b08
JustKiran-git <justkiran0406@gmail.com>
2024-08-05 09:26:43 +0530

    Update Numpynote.py

--- Numpynote.py
+++ Numpynote.py
@@ -104,6 +104,37 @@ for i in range(n):
 
 print("*"*60)
  
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("income.csv")
+print (df)
+
+
+income_dc = df.columns #all data columns from the income dataset
+print (income_dc)#first 2 columns from the income dataset
+print (income_dc [ :2])#find datatypes for all the columns
+print (df.dtypes)
+
+df.Y2008 = df.Y2008.astype (float) #converts integer type into float datatype
+print (df.dtypes)
+print ("total number of rows and columns:", df. shape)
+print ("total number of rows:", df. shape [0])
+print ("total number of columns:", df. shape [1])
+
+print ("First Five Rows from income dataset")
+print (df.head () )
+
+print ("Last Five Rows from income dataset")
+print (df.tail () )
+
+print ("First three")
+print (df. head (3) )
+
+print ("Last three Rows from income dataset")
+print (df.tail (3) )
+print (df.iloc[0:5])
+print (df[0:5])
 
 
 
